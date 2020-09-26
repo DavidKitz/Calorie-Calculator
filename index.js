a2ca885edd7a0af8a5447c2c0479292526a6ab5c
David Kitz <david.kitzwork@gmail.com>
2020-09-26 13:47:21 +0200

    new Api used

--- index.js
+++ index.js
@@ -15,26 +15,23 @@ app.get("/results",(req,res)=> {
 app.get("/information", (req,res)=> {
 let food=req.query.search;
 
-    axios({
-        "method":"GET",
-        "url":"https://nutritionix-api.p.rapidapi.com/v1_1/search/${food}",
-        "headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"nutritionix-api.p.rapidapi.com",
-        "x-rapidapi-key":"1d8617a7bemsh3a5cef89dcc2608p1841e6jsnf4c58e7a3b87",
-        "useQueryString":true
-        },
-        "params":{
-          "fields":"nf_calories"
-          
-        
-      }
-        })
+axios({
+  "method":"GET",
+  "url":"https://food-calorie-data-search.p.rapidapi.com/api/search",
+  "headers":{
+  "content-type":"application/octet-stream",
+  "x-rapidapi-host":"food-calorie-data-search.p.rapidapi.com",
+  "x-rapidapi-key":"1d8617a7bemsh3a5cef89dcc2608p1841e6jsnf4c58e7a3b87",
+  "useQueryString":true
+  },"params":{
+  "keyword":food
+  }
+  })
         .then((response)=>{
             let parser=JSON.stringify(response.data);
             
-          let dataSet=response.data.hits;
-          console.log(response.data.hits[0].fields)
+          let dataSet=response.data;
+          console.log(response.data)
           res.render("results", {dataSet:dataSet, food:food});
 
         })
